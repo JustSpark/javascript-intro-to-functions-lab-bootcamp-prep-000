806c71387f971db8a16e46abc94b6218e26ceebc
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-09 23:28:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === 'I love u, Grandma')
-  console.log('I love you, too.')
-  if (string === string.toLowerCase()) 
+  console.log('I love you, too.') }
+  {if (string === string.toLowerCase()) 
   return "I can't hear you!" 
   else if (string === string.toUpperCase())
   return "YES INDEED!"
