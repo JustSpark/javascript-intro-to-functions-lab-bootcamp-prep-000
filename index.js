64f71758ac5ec70db93d739330fa50605e31ced7
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-09 22:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,17 +4,3 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-var uppercase = "HELLO!"
-uppercase.toUpperCase() === uppercase
-var lowercase = "hello!" 
-lowercase.toLowercase() === lowercase
-var mixedCase = 'Hi there!'
-mixedCase.toLowerCase() === mixedCase
-mixedCase.toUpperCase() === mixedCase
- const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
