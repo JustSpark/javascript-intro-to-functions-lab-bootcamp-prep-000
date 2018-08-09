48e5471c1e25caed00baf12eec7322f25ea08a62
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-09 22:41:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,6 @@ function sayHiToGrandma(string) {
   return "I can't hear you!" 
   else if (string === string.toUpperCase())
   return "YES INDEED!"
+  else if (string === "I love u, Grandma.")
+  return "I love you too."
 }
