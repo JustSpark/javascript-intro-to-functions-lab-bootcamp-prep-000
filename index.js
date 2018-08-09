35485a5d8e0dc3fa06a8679e2e18f095f59d6d33
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-09 22:17:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase
+var lowercase = "hello!" 
+lowercase.toLowercase() === lowercase
